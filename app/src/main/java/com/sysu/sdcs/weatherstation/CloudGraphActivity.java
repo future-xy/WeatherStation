480e7e63c4a90c7cdc4845c10d9453ccb41d00d7
future-xy <fy38607203@163.com>
2020-07-17 22:18:09 +0800

    Debug

--- app/src/main/java/com/sysu/sdcs/weatherstation/CloudGraphActivity.java
+++ app/src/main/java/com/sysu/sdcs/weatherstation/CloudGraphActivity.java
@@ -12,6 +12,7 @@ import android.icu.util.TimeZone;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
@@ -51,12 +52,14 @@ public class CloudGraphActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cloud_graph);
         Calendar calendar = Calendar.getInstance();
         actionBar = getSupportActionBar();
+        if (actionBar == null)
+            Log.d("isNULL", "m");
         assert actionBar != null;
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(calendar.getTime().toLocaleString()+" "+getResources().getString(R.string.cloud_graph));
+        actionBar.setTitle(calendar.getTime().toLocaleString() + " " + getResources().getString(R.string.cloud_graph));
 
         cg_buffer = new ConcurrentHashMap<>();
         imageView = findViewById(R.id.cg_image);
@@ -79,12 +82,14 @@ public class CloudGraphActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                download_disp(urls, seekBar.getMax()-progress);
+                download_disp(urls, seekBar.getMax() - progress);
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
@@ -96,17 +101,17 @@ public class CloudGraphActivity extends AppCompatActivity {
                 if (cg_buffer.isEmpty()) return false;
                 seekBar.setProgress(seekBar.getMin());
                 Timer timer = new Timer();
-                for (int i = seekBar.getMin(); i<=seekBar.getMax(); i++) {
+                for (int i = seekBar.getMin(); i <= seekBar.getMax(); i++) {
                     TimerTask task = new TimerTask() {
                         @Override
                         public void run() {
                             int pro = seekBar.getProgress();
                             if (pro + 1 <= seekBar.getMax()) {
-                                seekBar.setProgress(pro+1);
+                                seekBar.setProgress(pro + 1);
                             }
                         }
                     };
-                    timer.schedule(task, 500*(i-seekBar.getMin()+1));
+                    timer.schedule(task, 500 * (i - seekBar.getMin() + 1));
                 }
                 return true;
             }
@@ -121,16 +126,24 @@ public class CloudGraphActivity extends AppCompatActivity {
         CG_URL_Handler handler = new CG_URL_Handler(calendar);
         int id;
         switch (radioGroup.getCheckedRadioButtonId()) {
-            case R.id.ultra_red: id=1; break;
-            case R.id.vis_color: id=2; break;
-            case R.id.steam: id=3; break;
-            default: id=0; break;
+            case R.id.ultra_red:
+                id = 1;
+                break;
+            case R.id.vis_color:
+                id = 2;
+                break;
+            case R.id.steam:
+                id = 3;
+                break;
+            default:
+                id = 0;
+                break;
         }
         urls = handler.generate(10, id);
-        for (int i = urls.size()-1; i>0; i--) {
+        for (int i = urls.size() - 1; i > 0; i--) {
             download_disp(urls, i);
         }
-        download_disp(urls, seekBar.getMax()-seekBar.getProgress());
+        download_disp(urls, seekBar.getMax() - seekBar.getProgress());
     }
 
     public void download_disp(ArrayList<String> urls, int progress) {
@@ -155,7 +168,7 @@ public class CloudGraphActivity extends AppCompatActivity {
                     Bitmap r_bitmap = getBitmapFromURL(target);
                     if (r_bitmap != null) {
                         Matrix matrix = new Matrix();
-                        matrix.setRotate(90, (float) r_bitmap.getWidth()/2, (float) r_bitmap.getHeight()/2);
+                        matrix.setRotate(90, (float) r_bitmap.getWidth() / 2, (float) r_bitmap.getHeight() / 2);
                         Bitmap bitmap = Bitmap.createBitmap(r_bitmap, 0, 0, r_bitmap.getWidth(), r_bitmap.getHeight(), matrix, true);
                         Message message = new Message();
                         message.what = 0;
@@ -187,24 +200,32 @@ public class CloudGraphActivity extends AppCompatActivity {
     }
 
     static class CG_URL_Handler {
-        private String[] url_pattern = {"http://image.nmc.cn/product/","/WXBL/SEVP_NSMC_WXBL_FY4A_ETCC_ACHN_LNO_PY_","00000.JPG?v="};
+        private String[] url_pattern = {"http://image.nmc.cn/product/", "/WXBL/SEVP_NSMC_WXBL_FY4A_ETCC_ACHN_LNO_PY_", "00000.JPG?v="};
         private String[] minutes = {"00", "15", "30", "45"};
         public int year, month, day, c_hour, c_minute;
 
         CG_URL_Handler(Calendar calendar) {
             year = calendar.get(Calendar.YEAR);
-            month = calendar.get(Calendar.MONTH)+1;
+            month = calendar.get(Calendar.MONTH) + 1;
             day = calendar.get(Calendar.DATE);
             c_hour = calendar.get(Calendar.HOUR_OF_DAY);
-            c_minute = calendar.get(Calendar.MINUTE)/15;
+            c_minute = calendar.get(Calendar.MINUTE) / 15;
         }
 
         public ArrayList<String> generate(int count, int type) {
             switch (type) {
-                case 1: url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_EC012_ACHN_LNO_PY_"; break;
-                case 2: url_pattern[1] = "/WXBL/medium/SEVP_NSMC_WXBL_FY4A_EC001_ACHN_LNO_PY_"; break;
-                case 3: url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_EC009_ACHN_LNO_PY_"; break;
-                default: url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_ETCC_ACHN_LNO_PY_"; break;
+                case 1:
+                    url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_EC012_ACHN_LNO_PY_";
+                    break;
+                case 2:
+                    url_pattern[1] = "/WXBL/medium/SEVP_NSMC_WXBL_FY4A_EC001_ACHN_LNO_PY_";
+                    break;
+                case 3:
+                    url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_EC009_ACHN_LNO_PY_";
+                    break;
+                default:
+                    url_pattern[1] = "/WXBL/SEVP_NSMC_WXBL_FY4A_ETCC_ACHN_LNO_PY_";
+                    break;
             }
             return generate(count);
         }
@@ -213,14 +234,14 @@ public class CloudGraphActivity extends AppCompatActivity {
             int i = 0;
             ArrayList<String> res = new ArrayList<>();
             int hour = c_hour, minute = c_minute;
-            minute = minute==0?3:minute-1;
-            hour = minute==3?hour-1:hour;
+            minute = minute == 0 ? 3 : minute - 1;
+            hour = minute == 3 ? hour - 1 : hour;
             while (i < count) {
-                if (c_hour<0) break;
-                minute = minute==0?3:minute-1;
-                hour = minute==3?hour-1:hour;
+                if (c_hour < 0) break;
+                minute = minute == 0 ? 3 : minute - 1;
+                hour = minute == 3 ? hour - 1 : hour;
                 Calendar t_calendar = Calendar.getInstance(TimeZone.GMT_ZONE);
-                t_calendar.set(year, month, day, hour, minute*15, 0);
+                t_calendar.set(year, month, day, hour, minute * 15, 0);
                 String builder = url_pattern[0] +
                         String.format("%4d/%02d/%02d", year, month, day) +
                         url_pattern[1] +
--- app/src/main/java/com/sysu/sdcs/weatherstation/MainActivity.java
+++ app/src/main/java/com/sysu/sdcs/weatherstation/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -65,6 +68,18 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
         actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setTitle("天气屋");
         */
+
+        //测试云图button，正式版删掉
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CloudGraphActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         cities = new Cities();
         HeWeatherConfig.init(HWKEY, curCity);//UI SDK
         HeConfig.init(HWID, HWKEY);//DATA SDK
