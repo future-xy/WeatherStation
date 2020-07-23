d63f0b0a36c73a3e1ef3c7341f9f88dbc53438ea
FunnyExplorer <37206522+FunnyExplorer@users.noreply.github.com>
2020-07-23 11:35:18 +0800

    Add files via upload

--- app/src/main/java/com/sysu/sdcs/weatherstation/AddCityActivity.java
+++ app/src/main/java/com/sysu/sdcs/weatherstation/AddCityActivity.java
@@ -46,30 +46,30 @@ public class AddCityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcity);
 
-        HeConfig.init("HE2007222112481749", "a737d0e84c084b84ba41b547cf25b98a");
-        HeConfig.switchToDevService();
+//        HeConfig.init("HE2007222112481749", "a737d0e84c084b84ba41b547cf25b98a");
+//        HeConfig.switchToDevService();
         //HeConfig.init(""); //("@string/PublicId", "@string/AppKey");
 
         weatherDb = new WeatherDB(this);
         context = this;
-        lis = new ArrayList<>();
-        mSearchView = findViewById(R.id.searchView);
+        lis = new ArrayList<GeoBean.LocationBean>();
+        mSearchView = (SearchView) findViewById(R.id.searchView);
         mSearchView.setIconifiedByDefault(false);
-        mListView = findViewById(R.id.searchlistView);
-        mStrs = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStrs);
+        mListView = (ListView) findViewById(R.id.searchlistView);
+        mStrs = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStrs);
         mListView.setAdapter(adapter);
         mListView.setTextFilterEnabled(true);
 
         // 设置搜索结果listview监听
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 GeoBean.LocationBean tmp = lis.get(position);
                 getTempandInsert(tmp.getId(), tmp.getName());
                 Intent data = new Intent();
                 //resultCode是返回码,用来确定是哪个页面传来的数据，这里设置返回码是2
-                setResult(1, data);
+                setResult(1,data);
                 finish();
             }
         });
@@ -85,21 +85,8 @@ public class AddCityActivity extends AppCompatActivity {
             // 当搜索内容改变时触发该方法
             @Override
             public boolean onQueryTextChange(String newText) {
-//                mStrs.clear();
-//                mStrs.add("广州");
-//                mStrs.add("白云");
-//                mStrs.add("番禺");
-//                mStrs.add("越秀");
-//                mStrs.add("增城");
-//                mStrs.add("海珠");
-//                mStrs.add("天河");
-//                mStrs.add("花都");
-//                mStrs.add("从化");
-//                mStrs.add("黄埔");
-                mStrs.clear();
-                mStrs.add("广州");
-
-                HeWeather.getGeoCityLookup(context, newText, Mode.FUZZY, Range.WORLD, 1, Lang.ZH_HANS, new HeWeather.OnResultGeoListener() {
+                // 调用API模糊搜索城市信息
+                HeWeather.getGeoCityLookup(context, newText, Mode.FUZZY, Range.WORLD, 10, Lang.ZH_HANS, new HeWeather.OnResultGeoListener() {
                     @Override
                     public void onError(Throwable throwable) {
                         Log.i("err", "getGeoCityLookup onError: " + throwable);
@@ -107,15 +94,16 @@ public class AddCityActivity extends AppCompatActivity {
 
                     @Override
                     public void onSuccess(GeoBean geoBean) {
-                        Log.i("res", "getGeoCityLookup onSuccess: " + new Gson().toJson(geoBean));
                         //先判断返回的status是否正确，当status正确时获取数据，若status不正确，可查看status对应的Code值找到原因
                         if (Code.OK.getCode().equalsIgnoreCase(geoBean.getStatus())) {
-                            System.out.println("****************enter success case");
-
                             lis.clear();
                             List<GeoBean.LocationBean> tmpLis = geoBean.getLocationBean();
-                            //mStrs.add(tmpLis.get(i).getName());
                             lis.addAll(tmpLis);
+                            mStrs.clear();
+                            for(int i = 0; i < lis.size();i++){
+                                mStrs.add(lis.get(i).getName());
+                            }
+                            mListView.setAdapter(adapter);
 
                         } else {
                             //在此查看返回数据失败的原因
@@ -123,17 +111,17 @@ public class AddCityActivity extends AppCompatActivity {
                             Code code = Code.toEnum(status);
                             Log.i("res", "failed code: " + code);
                         }
-                        System.out.println("***************before finish onsuccess");
                     }
                 });
-                System.out.println("***************get out of heweather");
-                adapter.getFilter().filter(newText);//通过适配器过滤
-                System.out.println("***************before return");
+
+                //adapter.getFilter().filter(newText);//通过适配器过滤
                 return false;
             }
         });
+
     }
 
+    // 获取所选城市温度，插入数据库
     private void getTempandInsert(final String lid, final String name) {
         final String[] temperature = new String[1];
         HeWeather.getWeatherNow(this, lid, Lang.ZH_HANS, Unit.METRIC, new HeWeather.OnResultWeatherNowListener() {
@@ -149,16 +137,13 @@ public class AddCityActivity extends AppCompatActivity {
                 if (Code.OK.getCode().equalsIgnoreCase(weatherBean.getCode())) {
                     WeatherNowBean.NowBaseBean now = weatherBean.getNow();
                     temperature[0] = now.getTemp();
-                    System.out.println("******************the temp now:");
-                    System.out.println(now.getTemp());
-                    System.out.println(temperature[0]);
 
                     ContentValues cv = new ContentValues();
                     cv.put("LocationID", lid);
                     cv.put("City", name);
                     cv.put("Temperature", temperature[0]);
                     weatherDb.insert("WeatherNow", cv);
-                    System.out.println("insert successfully!!!");
+
                 } else {
                     //在此查看返回数据失败的原因
                     String status = weatherBean.getCode();
--- app/src/main/java/com/sysu/sdcs/weatherstation/CityList.java
+++ app/src/main/java/com/sysu/sdcs/weatherstation/CityList.java
@@ -10,6 +10,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -30,6 +32,16 @@ public class CityList extends AppCompatActivity {
     private ListView lv;
     private ArrayList<SimWea> weas;
 
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            if(msg.what==0x123){
+                Cursor cursor = weatherDb.query("WeatherNow", null, null, null, null);
+                listCites(cursor);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +51,13 @@ public class CityList extends AppCompatActivity {
         weatherDb = new WeatherDB(this);
 
         // 将增加城市按钮放到listview的最后一栏
-        View view = View.inflate(this, R.layout.add_button, null);
+        View view = View.inflate(CityList.this, R.layout.add_button, null);
         view.findViewById(R.id.addcity).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CityList.this, AddCityActivity.class);
                 //startActivity(intent);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent,1);
             }
         });
         lv.addFooterView(view);
@@ -54,15 +66,15 @@ public class CityList extends AppCompatActivity {
     }
 
     //列出所有城市及其温度
-    private void listCity() {
+    private void listCity(){
         Cursor cursor = weatherDb.query("WeatherNow", null, null, null, null);
         listCites(cursor);
     }
 
-    private void listCites(Cursor cursor) {
-        if (cursor != null) {
-            weas = new ArrayList<>();
-            while (cursor.moveToNext()) {
+    private void listCites(Cursor cursor){
+        if(cursor!=null){
+            weas = new ArrayList<SimWea>();
+            while(cursor.moveToNext()){
                 SimWea tmp = new SimWea();
                 tmp.setID(cursor.getString(cursor.getColumnIndex("LocationID")));
                 tmp.setCity(cursor.getString(cursor.getColumnIndex("City")));
@@ -73,7 +85,7 @@ public class CityList extends AppCompatActivity {
         }
 
         ArrayList<Map<String, Object>> data = new ArrayList<>();
-        for (int i = 0; i < weas.size(); i++) {
+        for(int i = 0; i < weas.size(); i++){
             Map<String, Object> map = new HashMap<>();
             map.put("City", weas.get(i).getCity());
             map.put("Temperature", weas.get(i).getTemp());
@@ -89,27 +101,26 @@ public class CityList extends AppCompatActivity {
         lv.setLayoutParams(lp);
 
         // 短按跳转到详细天气页面
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
 
             }
         });
         // 长按删除该城市
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
                 PopupMenu popup = new PopupMenu(CityList.this, view);
                 popup.getMenuInflater().inflate(R.menu.menu_popup, popup.getMenu());
                 final SimWea tmp = weas.get(position);
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
+                        switch(item.getItemId()){
                             case R.id.del:
                                 delCity(tmp);
                                 break;
-                            default:
-                                break;
+                            default:break;
                         }
                         return true;
                     }
@@ -120,27 +131,35 @@ public class CityList extends AppCompatActivity {
         });
     }
 
-    private void delCity(SimWea city) {
+    // 删除城市
+    private void delCity(SimWea city){
         String id = city.getID();
         int ret = weatherDb.delete("WeatherNow", "LocationID=?", new String[]{String.valueOf(id)});
         Toast.makeText(this, "删除成功", Toast.LENGTH_SHORT).show();
         Cursor newCursor = weatherDb.query("WeatherNow", null, null, null, null);
         listCites(newCursor);
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == 1) {//当请求码是1&&返回码是1进行下面操作
-            TimerTask task = new TimerTask() {
+    @Override  // 用于响应AddCityActivity的跳转回复
+    protected void onActivityResult(int requestCode,int resultCode ,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1&&resultCode==1){//当请求码是1&&返回码是1进行下面操作
+            new Thread(){
                 @Override
-                public void run() {
-                    Cursor cursor = weatherDb.query("WeatherNow", null, null, null, null);
-                    listCites(cursor);
+                public void run(){
+                    try{
+                        // 刷新城市列表，等待API调用完毕
+                        for (int i=0; i < 5; i++){
+                            handler.sendEmptyMessage(0x123);
+                            Thread.sleep(2000);
+                        }
+                    }catch(Exception ex){
+                        ex.printStackTrace();
+                    }
                 }
-            };
-            Timer timer = new Timer();
-            timer.schedule(task, 2000);//3秒后执行TimeTask的run方法
+            }.start();
         }
     }
+
 }
