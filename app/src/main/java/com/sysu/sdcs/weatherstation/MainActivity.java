ccf3a40cf57cb65333f4f9046220b175dd2b92de
1023198294 <1023198294@qq.com>
2020-07-24 13:06:30 +0800

    optimize the permission request
    Signed-off-by: Huang Yangjun <1023198294@qq.com>

--- app/src/main/java/com/sysu/sdcs/weatherstation/MainActivity.java
+++ app/src/main/java/com/sysu/sdcs/weatherstation/MainActivity.java
@@ -106,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.this.deleteDatabase("dbWeather.db3");
         mainActivity = this;
         /*
         太丑了，感觉没必要
@@ -241,9 +242,10 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
     }
 
     String getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED | ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "getLocation: permission");
-            Toast.makeText(MainActivity.this, "请开启位置(GPS)权限", Toast.LENGTH_LONG).show();
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            //Toast.makeText(MainActivity.this, "请开启位置(GPS)权限", Toast.LENGTH_LONG).show();
             return "";
         }
         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
